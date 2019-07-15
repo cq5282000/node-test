929835353f4c12a27c8ca9333cd09fdccbc86149
cq5282000 <18801911877@163.com>
2019-07-15 14:22:58 +0800

    dd

--- test/constructorGraphTest.js
+++ test/constructorGraphTest.js
@@ -1,5 +1,8 @@
 const SymbolGraph = require('../src/SymbolGraph');
+const DirectedCycle = require('../src/DirectedCycle');
 const data = require('./data.json');
-const sg = new SymbolGraph(data);
 
-console.log(sg.G.adj);
+const sg = new SymbolGraph(data);
+const dc = new DirectedCycle(sg.G);
+
+console.log(dc.getCycle());
